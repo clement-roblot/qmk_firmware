5eeafed041316351d85e0425641a0c10827debdb
Clément Roblot <clement.roblot@martobre.fr>
2020-07-22 14:25:05 +0700

    Cleaned the alignment

--- keyboards/cytokinesis/rules.mk
+++ keyboards/cytokinesis/rules.mk
@@ -20,28 +20,29 @@ MCU_LDSCRIPT = NRF52840
 # The CORTEX_VTOR... is needed only for MCHCK/Infinity KB
 OPT_DEFS += -DCORTEX_VTOR_INIT=0x00002000
 
-BOOTLOADER = dfu
+BOOTLOADER = nrfutil
 
 # Build Options
 #   comment out to disable the options.
 #
 
-DFU_ARGS = -d 1c11:b007
-DFU_SUFFIX_ARGS = -p b007 -v 1c11
+DFU_ARGS            = -d 1c11:b007
+DFU_SUFFIX_ARGS     = -p b007 -v 1c11
 
-BOOTMAGIC_ENABLE = no  # Virtual DIP switch configuration
-MOUSEKEY_ENABLE  = yes # Mouse keys
-EXTRAKEY_ENABLE  = yes # Audio control and System control
-CONSOLE_ENABLE   = no  # Console for debug
-COMMAND_ENABLE   = yes # Commands for debug and configuration
-CUSTOM_MATRIX    = yes # Custom matrix file for the ErgoDox EZ
-SLEEP_LED_ENABLE = yes # Breathing sleep LED during USB suspend
-NKRO_ENABLE      = yes # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-UNICODE_ENABLE   = yes # Unicode
-SWAP_HANDS_ENABLE= yes # Allow swapping hands of keyboard
+BOOTMAGIC_ENABLE    = no  # Virtual DIP switch configuration
+MOUSEKEY_ENABLE     = yes # Mouse keys
+EXTRAKEY_ENABLE     = yes # Audio control and System control
+CONSOLE_ENABLE      = no  # Console for debug
+COMMAND_ENABLE      = yes # Commands for debug and configuration
+CUSTOM_MATRIX       = yes # Custom matrix file for the ErgoDox EZ
+SLEEP_LED_ENABLE    = yes # Breathing sleep LED during USB suspend
+NKRO_ENABLE         = yes # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+UNICODE_ENABLE      = yes # Unicode
+SWAP_HANDS_ENABLE   = yes # Allow swapping hands of keyboard
 
-CUSTOM_MATRIX = yes # Custom matrix file
-SERIAL_LINK_ENABLE = yes
+CUSTOM_MATRIX       = yes # Custom matrix file
+WAIT_FOR_USB        = no
+SERIAL_LINK_ENABLE  = no
 # VISUALIZER_ENABLE = yes
 # LCD_ENABLE = yes
 # BACKLIGHT_ENABLE = yes
